b1891e1a84018f6571d5f889f6624c9d40be1bab
Mehdichaaki <mehdichaaki@gmail.com>
2023-10-24 14:54:00 +0100

    project

---
+++ page/src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/Home.js";
+import Login from "./pages/login/Login.js";
+import Profile from "./pages/profile/Profile.js";
+import Register from "./pages/register/Register.js";
+
+function App() {
+  return <Profile/>;
+}
+
+export default App;
